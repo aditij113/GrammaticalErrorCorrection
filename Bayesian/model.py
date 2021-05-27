a2bb5c3f9df52c1b1922db79538cd1853bec9dd5
Aditi <aditiajain@gmail.com>
2021-05-26 22:13:43 -0700

    Classify CONLL test file

--- Bayesian/model.py
+++ Bayesian/model.py
@@ -2,12 +2,13 @@ import operator
 import nltk
 from nltk.corpus import brown
 from nltk.probability import *
+import sys
 
 class BayesianModel:
     """Model that predicts whether or not a sentence has a 
        grammatical error based on n-gram prior probabilities."""
 
-    def __init__(self, n: int, threshold: int):
+    def __init__(self, n: int, threshold: float):
         self.n = n
         self.threshold = threshold
 
@@ -31,21 +32,44 @@ class BayesianModel:
         score = 0
         for bigram in bigrams:
             # Conside Laplace: laplace = LaplaceProbDist(cfdist[word], 20000)
-            print("bigram: ", bigram)
             mle = MLEProbDist(self.cfdist[bigram[0]])
             score += mle.prob(bigram[1])
-            print("bigram score: ", mle.prob(bigram[1]))
-        return score
+        return score < self.threshold
+
+    # Should be in format sentence,label
+    # Returns precision for pos and neg classes.
+    def classifyFile(self, filename):
+        tp, tn, fp, fn = 0, 0, 0, 0
+        with open(filename) as f:
+            line = f.readline()
+            while line:
+                label = int(line.split(',')[-1])
+                sentence = line[:-2]
+                pred = self.classify(sentence)
+                if label == 1:
+                    if pred == 1: tp += 1
+                    if pred == 0: fn += 1
+                if label == 0:
+                    if pred == 0: tn += 1
+                    if pred == 1: fp += 1
+                line = f.readline()
+        return tp / (tp + fn), tn / (tn + fp)
 
 if __name__ == '__main__':
     print("number of sentences: ", len(brown.sents()))
     print("number of words: ", len(brown.words()))
-    model = BayesianModel(0, 0)
+    model = BayesianModel(2, 0.3)
     model.build_priors()
-    while True:
-        sentence = input("Enter sentence: ")
-        score = model.classify(sentence)
-        print("Score is: ", score)
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        p, n = model.classifyFile(filename)
+        print("Positive class precision: ", p)
+        print("Negative class precision: ", n)
+    else:  # Try out sentences.
+        while True:
+            sentence = input("Enter sentence: ")
+            pred  = model.classify(sentence)
+            print("Pred is: ", pred)
         
     
     
